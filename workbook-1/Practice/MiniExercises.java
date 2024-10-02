2e6c7e1ccdc83e3f37dcc35115caf72cf230c80f
Adrian <adrianchristiansanmiguel@gmail.com>
2024-10-01 20:04:55 -0700

    Fix

---
+++ workbook-1/Practice/MiniExercises.java
@@ -0,0 +1,59 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class MiniExercises {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String name = "Adrian";
+        int age = 27;
+        String phoneNumber = "650-243-8999";
+        boolean IsWeatherNice = false;
+        String weight = "195lbs";
+        System.out.println("What is the radius of your table? : ");
+        double r = scanner.nextDouble();
+        double pi = Math.PI; // was not working
+        double cf = r * 2 * 3.14; // tired cf = Math.PI(r*2) but error
+        System.out.printf("Your table's circumference is " + "%.2f", cf);
+        scanner.nextLine();
+
+        System.out.println("\nWhat is your name? : ");
+        String yourName = scanner.nextLine();
+        for (int i = yourName.length(); i <= yourName.length(); i++) {
+            char character = yourName.charAt(0);
+            System.out.println(character);
+        }
+
+        String answerToLife = "Change";
+        int [] rando = {1,2,3,4,5,6,7,8,9,10};
+        System.out.printf("This is your random number " + String.valueOf(Math.random())); //idk how to do this yet
+
+
+        System.out.println("\nWhat length is one side of your triangle? : ");
+        double side1 = scanner.nextDouble();
+        double a = Math.pow(side1, 2);
+        System.out.println("What length is the second side of your triangle? : ");
+        double side2 = scanner.nextDouble();
+        double b = Math.pow(side2, 2);
+        double c = Math.sqrt(a + b);
+        System.out.printf("The hypotenuse of your triangle is : %.2f", c);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ workbook-2/com.pluralsight/FullNameParser.java
@@ -0,0 +1,34 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class FullNameParser {
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Welcome All!");
+        System.out.println("Please choose a format to enter your name: \n" +
+                "Option 1 : First, Last \n" +
+                "Option 2 : First, Middle, Last \n" +
+                "Enter 1 or 2 : ");
+        String nameTypeOption = scanner.nextLine();
+        String fName = input("First Name : ").trim();
+        String lName = input("Last Name : ").trim();
+
+        if (nameTypeOption.equals("1")) {
+            String wholeName = fName + " " + lName;
+            System.out.println("Hello! " + wholeName);
+        } else {
+            String mName = input("Middle Name : ").trim();
+            String wholeName = fName + " " + mName + " " + lName;
+            System.out.println("Hello! " + wholeName);
+
+        }
+    }
+    public static String input(String message) {
+        System.out.println("Please Enter Your " + message);
+        return scanner.nextLine();
+    }
+
+}
+
+
