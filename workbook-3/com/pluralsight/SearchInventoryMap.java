7cbe6786b07cbe41900fb4777dd3b47c962c1d7e
Adrian <adrianchristiansanmiguel@gmail.com>
2024-10-11 10:37:44 -0700

    Organization

--- workbook-3/com/pluralsight/SearchInventoryMap.java
+++ workbook-3/com/pluralsight/SearchInventoryMap.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.HashMap;
+import java.util.regex.Pattern;
 
 
 public class SearchInventoryMap {
@@ -15,36 +16,35 @@ public class SearchInventoryMap {
         try {
             FileReader fileReader = new FileReader("DataFiles/" + "inventory.csv");
             BufferedReader bufReader = new BufferedReader(fileReader);
+             Map<Integer, Product> inventory = new HashMap<>();
 
-            String input;
+            String input = bufReader.readLine();
             String last;
 
             while ((input = bufReader.readLine()) != null) {
                 String[] arrInventory = input.split("\\|");
                 int productId = Integer.parseInt(arrInventory[0]);
                 Product product1 = new Product(productId, arrInventory[1], Float.parseFloat(arrInventory[2]));
-                Map<Integer, Product> inventory = new HashMap<>();
                 inventory.put(productId, product1);
 
-              inventory.put(productId,product1);
                 last = inventory.toString();
 
                 Scanner scanner = new Scanner(System.in);
                 System.out.print("Please enter product id of the product you would like to search: ");
                 int id = scanner.nextInt();
                 scanner.nextLine();
-                Product matchedProduct = inventory.get(id);
+                Product matchedProduct = inventory.get(productId);
                 if (matchedProduct == null) {
                     System.out.println("We don't carry that product");
                     scanner.nextLine();
-                    inventory.put(productId, product1);
                 }
-                System.out.printf("We carry %s and the price is $%.2f\n",
+                System.out.printf("Product: %s  Price: $%.2f\n",
                         matchedProduct.getName(), matchedProduct.getPrice());
             }
+            bufReader.close();
             scanner.close();
         } catch (Exception e) {
-            System.out.println("No File Found");
+            System.out.println("Input Error");
             e.printStackTrace();
        }
     }
