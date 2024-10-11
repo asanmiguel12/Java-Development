1923774a4596f84a8662b834af8e2567d68cb21b
Adrian <adrianchristiansanmiguel@gmail.com>
2024-10-11 09:27:22 -0700

    Added SearchInventory

--- workbook-3/Mod01/PayRollCalculator.java
+++ workbook-3/Mod01/PayRollCalculator.java
@@ -10,27 +10,27 @@ public class PayRollCalculator {
            // prompt user for the name of the file
             System.out.println("Enter the name of the file employee file you would like to read: ");
             String empFileName = scanner.nextLine();
-            System.out.println("Enter the name of the payroll file you want to create: payroll-sept-2023.csv");
-            String chooseFileName = scanner.nextLine();
+//            System.out.println("Enter the name of the payroll file you want to create: payroll-sept-2023.csv");
+//            String chooseFileName = scanner.nextLine();
 
         try {
-            FileReader employees = new FileReader("DataFiles/" + empFileName);
+            FileReader employees = new FileReader("DataFiles/" + "employees.csv");
             BufferedReader bufReader = new BufferedReader(employees);
-            FileWriter fileWriter = new FileWriter(chooseFileName);
-            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            // FileWriter fileWriter = new FileWriter(chooseFileName);
+            // BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
 
             //List<String> employeeDataList = new ArrayList<>();
-            String input;
+            String input = bufReader.readLine();
 
             while ((input = bufReader.readLine()) != null) {
                 String[] inputformatted = input.split("\\|");
                 Employee employee1 = new Employee(Double.parseDouble(inputformatted[0]), inputformatted[1], Double.parseDouble(inputformatted[2]), Double.parseDouble(inputformatted[3]));
 
-                String formattedEmployee = "Employee: " + employee1.getEmployeeId() + " Name: " + employee1.getName() + " Gross Pay: $" + employee1.getGrosspay() ;
+               // String formattedEmployee = "Employee: " + employee1.getEmployeeId() + " Name: " + employee1.getName() + " Gross Pay: $" + employee1.getGrosspay() ;
                 System.out.println("Employee: " + employee1.getEmployeeId() + " Name: " + employee1.getName() + " Gross Pay: $" + employee1.getGrosspay());
-                fileWriter.write(formattedEmployee);
+                //fileWriter.write(formattedEmployee);
             }
-            fileWriter.close();
+            //fileWriter.close();
             bufReader.close();
         } catch (IOException e) {
             System.out.println(e.getMessage());
--- workbook-3/com/pluralsight/SearchInventoryMap.java
+++ workbook-3/com/pluralsight/SearchInventoryMap.java
@@ -1,30 +1,51 @@
 package com.pluralsight;
 
+import java.io.*;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.HashMap;
+
+
 public class SearchInventoryMap {
-    public static void main(String[] args) {
-
-
-
-
-
-
-
-    }
-
-    static void loadInventory(String[] args) {
-    // this method loads product objects into inventory
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
         loadInventory();
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("What item # are you interested in? ");
-        int id = scanner.nextInt();
-        Product matchedProduct = inventory.get(id);
-        if (matchedProduct == null) {
-            System.out.println("We don't carry that product");
-            return;
-        }
-        System.out.printf("We carry %s and the price is $%.2f",
-                matchedProduct.getName(), matchedProduct.getPrice());
+    }
+    static void loadInventory() {
+        try {
+            FileReader fileReader = new FileReader("DataFiles/" + "inventory.csv");
+            BufferedReader bufReader = new BufferedReader(fileReader);
+
+            String input;
+            String last;
+
+            while ((input = bufReader.readLine()) != null) {
+                String[] arrInventory = input.split("\\|");
+                int productId = Integer.parseInt(arrInventory[0]);
+                Product product1 = new Product(productId, arrInventory[1], Float.parseFloat(arrInventory[2]));
+                Map<Integer, Product> inventory = new HashMap<>();
+                inventory.put(productId, product1);
+
+              inventory.put(productId,product1);
+                last = inventory.toString();
+
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Please enter product id of the product you would like to search: ");
+                int id = scanner.nextInt();
+                scanner.nextLine();
+                Product matchedProduct = inventory.get(id);
+                if (matchedProduct == null) {
+                    System.out.println("We don't carry that product");
+                    scanner.nextLine();
+                    inventory.put(productId, product1);
+                }
+                System.out.printf("We carry %s and the price is $%.2f\n",
+                        matchedProduct.getName(), matchedProduct.getPrice());
+            }
+            scanner.close();
+        } catch (Exception e) {
+            System.out.println("No File Found");
+            e.printStackTrace();
+       }
     }
 }
-
-
