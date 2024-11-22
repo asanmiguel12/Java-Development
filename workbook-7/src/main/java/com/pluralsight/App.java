e4ef21a439224427302f258c2a879a562756b420
Adrian San Miguel <adrianchristiansanmiguel@gmail.com>
2024-11-22 10:43:33 -0800

    Added DatabaseConnection to pom workbook 8 and tested in Mod3

---
+++ workbook-7/src/main/java/com/pluralsight/App.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+public class App {
+
+    public static void main( String[] args ) {
+
+        System.out.println( "Hello World!" );
+
+
+    }
+}
---
+++ workbook-8/src/main/java/com/pluralsight/Mod3.java
@@ -0,0 +1,20 @@
+package com.pluralsight;
+
+import java.sql.*;
+
+public class Mod3 {
+    public static void main(String[] args) throws SQLException {
+
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", "root", "");
+
+        Statement statement = connection.createStatement();
+        // execute query
+        String query = "SELECT productname FROM products";
+        ResultSet rs = statement.executeQuery(query);
+        // process results
+        while(rs.next()) {
+            System.out.println(rs.getString("productname"));
+        }
+        connection.close();
+    }
+}
